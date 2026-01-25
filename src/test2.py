f61ab2ab835cdd2fade8c04f3cd7e8bd4c249c2d
Kai Quach <kaiquach04@gmail.com>
2026-01-25 14:38:10 -0600

    img

--- src/test2.py
+++ src/test2.py
@@ -1,10 +1,33 @@
+import argparse
+import pathlib
+import sys
+
+from PIL import Image
+
 from inky.auto import auto
-from PIL import Image, ImageFont, ImageDraw
-from font_fredoka_one import FredokaOne
 
+parser = argparse.ArgumentParser()
 
-inky_display = auto()
+parser.add_argument("--saturation", "-s", type=float, default=0.5, help="Colour palette saturation")
+parser.add_argument("--file", "-f", type=pathlib.Path, help="Image file")
 
-img = Image.open("../public/em.JPG")
-inky_display.set_image(img)
-inky_display.show()
+inky = auto(ask_user=True, verbose=True)
+
+args, _ = parser.parse_known_args()
+
+saturation = args.saturation
+
+if not args.file:
+    print(f"""Usage:
+    {sys.argv[0]} --file image.png (--saturation 0.5)""")
+    sys.exit(1)
+
+image = Image.open(args.file)
+resizedimage = image.resize(inky.resolution)
+
+try:
+    inky.set_image(resizedimage, saturation=saturation)
+except TypeError:
+    inky.set_image(resizedimage)
+
+inky.show()
