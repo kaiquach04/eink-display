ed04a945887783f898c7b38d1c9034016a373419
Kai Quach <kaiquach04@gmail.com>
2026-02-02 12:45:14 -0600

    file change

--- src/flask-eink.py
+++ src/flask-eink.py
@@ -269,4 +269,4 @@ def render_png():
 
 if __name__ == "__main__":
     render(WIDTH, HEIGHT)
-    app.run(host="localhost", port=5000, debug=True)
+    app.run(host="127.0.0.1", port=5000, debug=True)
