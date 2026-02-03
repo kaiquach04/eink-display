4555b71b15c30cff817b307141ee49d5ccb9cdf1
Kai Quach <kaiquach04@gmail.com>
2026-02-02 21:38:21 -0600

    spotify button

--- src/eink-main.py
+++ src/eink-main.py
@@ -1,21 +1,66 @@
 # eink_main.py
 import time
 from inky.auto import auto
-from calendar_logic import render, WIDTH, HEIGHT
+from calendar_logic import render as render_calendar
+from calendar_logic import WIDTH, HEIGHT
+import gpiod
+import gpiodevice
+# from spotify_logic import render as render_spotify
+from gpiod.line import Bias, Direction, Edge
+
+SW_A = 5
+SW_B = 6
+SW_C = 16
+SW_D = 24
+
+BUTTONS = [SW_A, SW_B, SW_C, SW_D]
+
+LABELS = ["A", "B", "C", "D"]
+
+INPUT = gpiod.LineSettings(direction=Direction.INPUT, bias=Bias.PULL_UP, edge_detection=Edge.FALLING)
+
+chip = gpiodevice.find_chip_by_platform()
+
+OFFSETS = [chip.line_offset_from_id(id) for id in BUTTONS]
+line_config = dict.fromkeys(OFFSETS, INPUT)
+
+request = chip.request_lines(consumer="spectra6-buttons", config=line_config)
 
 inky_display = auto()
+current_mode = "calendar"
+last_refresh_time = 0
+REFRESH_INTERVAL = 480
 
 def update_display():
-    print("Refreshing E-ink...")
-    img = render(WIDTH, HEIGHT)
+    global last_refresh_time
+    print(f"Refreshing E-ink in {current_mode} mode...")
+
+    if current_mode == "calendar":
+        img = render_calendar(WIDTH,HEIGHT)
+    elif current_mode == "spotify":
+        #img = render_spotify(WIDTH, HEIGHT)
+        print("Spotify mode selected (logic not done)")
+
     display_img = img.convert("RGB")
     inky_display.set_image(display_img)
     inky_display.show()
+    last_refresh_time = time.time()
 
 if __name__ == "__main__":
+    update_display()
     while True:
-        try:
+        events = request.wait_edge_events(dt.timedelta(seconds=1))
+        if events:
+            for event in events:
+                offset = event.line_offset
+                if offset == OFFSETS[0]:
+                    print("Button A: Switching to Calendar")
+                    current_mode = "calendar"
+                    update_display()
+                elif offset == OFFSETS[1]:
+                    print("Button B: Switching to Spotify")
+                    current_mode = "spotify"
+                    update_display()
+        if time.time() - last_refresh_time > REFRESH_INTERVAL:
+            print("Timer triggered: refresh started")
             update_display()
-        except Exception as e:
-            print(f"Update failed: {e}")
-        time.sleep(480) # Wait 8 minutes
